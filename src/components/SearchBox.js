0bf316d1cd068ea4aa8fe6a4a2e1790737e70157
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-04-17 23:16:40 +0900

    commit before deployment

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
-  return (
-    <div className='pa2'>
-      <input
-        className='pa3 ba b--green bg-lightest-blue'
-        type='search'
-        placeholder='search robots'
-        onChange={searchChange}
-      />
-    </div>
-  );
+    return (
+        <div className='pa3 ba b--green bg-lightest-blue'>
+        <input type='search'
+         placeholder='Search Robots' 
+         onChange={searchChange}
+         />
+         </div>
+    );
 }
 
 export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+//import files needed and other components created
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import reportWebVitals from './reportWebVitals';
+
+//create and manipulate the dom
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+);
 
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+reportWebVitals();
